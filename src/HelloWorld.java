0d52ad2c19da2511f35192133da6fea55f50cf29
serifebs <serifebs@outlook.com>
2022-04-03 13:49:39 +0100

    work 2 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 
     // Doing some work here
     // comment - soldaki yesil cizgi en son commit ile suan arasaindaki `change`i gosteriyor.
+    // tekrar commit yaptiktan sonra green bar gidiyor. Ama yeni degisiklik yaptikca tekrar gozukuyor.
 
 }
