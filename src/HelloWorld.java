4f5c395db7d039313cc227e7776436ca7ad90120
serifebs <serifebs@outlook.com>
2022-04-03 13:56:38 +0100

    work 3 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,5 @@ public class HelloWorld {
     // Doing some work here
     // comment - soldaki yesil cizgi en son commit ile suan arasaindaki `change`i gosteriyor.
     // tekrar commit yaptiktan sonra green bar gidiyor. Ama yeni degisiklik yaptikca tekrar gozukuyor.
-
+    // yaptigin commit`leri gormek icin sol alttaki "git" -> "log" a tikla.
 }
